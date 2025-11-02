3e88bf36b47c516ba8995e1bdf74d118e9cb8871
joonkiat.c <51284036+joon-kc@users.noreply.github.com>
2025-11-02 21:38:42 +0800

    updated for video llama 3.1

--- main/models/model.py
+++ main/models/model.py
@@ -5,19 +5,14 @@ import time
 
 from transformers import (
     AutoTokenizer, 
-    AutoProcessor,
-    AutoModelForCausalLM,
-    AutoModel,
-    AutoImageProcessor,
+    AutoProcessor, 
     Qwen2_5_VLForConditionalGeneration, 
     VideoLlavaForConditionalGeneration, 
     VideoLlavaProcessor
 )
 from qwen_vl_utils import process_vision_info
-
 from videollama2 import model_init, mm_infer
 from videollama2.utils import disable_torch_init
-
 
 ### ======================= Qwen2.5-VL-7B-Instruct =======================
 class QwenVL:
@@ -243,7 +238,7 @@ class VideoLlava:
         ### Run Generation of Inputs
         print("Inference Start")
         start = time.perf_counter()
-        with torch.no_grad(), torch.autocast("cuda", dtype=torch.bfloat16):
+        with torch.no_grad():
             generated_ids = self.model.generate(**inputs, max_new_tokens=max_new_tokens)
         generated_ids_trimmed = [
             out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
@@ -265,7 +260,7 @@ class VideoLlava:
         return output_text, f"{elapsed_time:.4f}"
 
 
-### ======================= DAMO-NLP-SG/VideoLLaMA2.1-7B-16F-Base =======================
+### ======================= DAMO-NLP-SG/VideoLLaMA2.1-7B-AV =======================
 class VideoLLama2:
     def __init__ (self, model_path: str):
         self.model_path = model_path
@@ -283,118 +278,45 @@ class VideoLLama2:
 
     def generate_prompt_message(self, text_prompt, video_paths) -> str:
         content = []
-        pass
-
-
-    ### Video Inference Only -- No Audio Inference
-    def run_inference(self, text_prompt: str, video_paths: list[str], max_new_tokens: int = 128, modality: str = "video"):
-        
-        if video_paths.isinstance(list):
-            assert len(video_paths) == 1
-            video_path = video_paths[0]
-        
-        ### Prepare for Inference
-        # preprocess = self.processor[modality]
-        # inputs = preprocess(video_path)
-        inputs = self.processor[modality](self.modal_path)
-
-        ### Run Generation of Inputs
-        print("Inference Start")
-        start = time.perf_counter()
-        
-        output_text = mm_infer(
-            inputs,
-            text_prompt, 
-            model=self.model, 
-            tokenizer=self.tokenizer, 
-            do_sample=False, 
-            modal=modality
-        )
-
-        print(f"Model response: {output_text}")
-        end = time.perf_counter()
-        print("Inference End")
-
-        # Free memory manually
-        del inputs
-        torch.cuda.empty_cache()
-        elapsed_time = end - start
-
-        return output_text, f"{elapsed_time:.4f}"
-    
-
-### ======================= DAMO-NLP-SG/VideoLLaMA3-7B =======================
-class VideoLLama3:
-    def __init__ (self, model_path: str):
-        self.model_path = model_path
-        self.fps = 1.0
-        self.num_frames = 1.0 # used for sampling the number of frames
-        self.max_frames = 256
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.model = self.load_model()
-        self.processor = self.load_processor()
-
-
-    def load_model(self):
-        model = AutoModelForCausalLM.from_pretrained(
-            self.model_path,
-            trust_remote_code=True,
-            device_map="auto",
-            torch_dtype=torch.bfloat16,
-            attn_implementation="flash_attention_2",
-        )
-        return model
-        
-    
-    def load_processor(self):
-        processor = AutoProcessor.from_pretrained(
-            self.model_path, 
-            trust_remote_code=True
-        )
-        return processor
-    
-
-    def generate_prompt_message(self, text_prompt, video_paths) -> str:
-        content = []
         for vpath in video_paths:
             content.append(
                 {
                     "type": "video",
-                    "video": {
-                        "video_path": vpath,
-                        "fps": self.fps,
-                        "max_frames": self.max_frames
-                    }
+                    "video": vpath,
+                    "fps": self.fps,
                 }
             )
         content.append({"type": "text", "text": text_prompt})
         messages = [
-            {"role": "system", "content": "You are a video answering assistant."},
             {
                 "role": "user",
                 "content": content,
             }
         ]
         return messages
+    
 
-
-    def run_inference(self, text_prompt: str, video_paths: list[str], max_new_tokens: int=128):
-
+    ### Video Inference Only -- No Audio Inference
+    def run_inference(self, text_prompt: str, video_paths: list[str], max_new_tokens: int = 128):
+        if video_paths.isinstance(list):
+            assert len(video_paths) == 1
+            video_path = video_paths[0]
+        
         ### Prepare for Inference
-        messages = self.generate_prompt_message(text_prompt, video_paths)
-        inputs = self.processor(conversation=messages, return_tensors="pt")
-        inputs = {k: v.cuda() if isinstance(v, torch.Tensor) else v for k, v in inputs.items()}
-        if "pixel_values" in inputs:
-            inputs["pixel_values"] = inputs["pixel_values"].to(torch.bfloat16)
+        preprocess = self.processor["video"]
+        audio_video_tensor = preprocess(video_path, va=False)
 
         ### Run Generation of Inputs
         print("Inference Start")
         start = time.perf_counter()
-        
-        with torch.no_grad(), torch.autocast("cuda", dtype=torch.bfloat16):
-            generated_ids = self.model.generate(**inputs, max_new_tokens=max_new_tokens)
-        output_text = self.processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
-
+        output_text = mm_infer(
+            audio_video_tensor,
+            text_prompt,
+            model=self.model,
+            tokenizer=self.tokenizer,
+            modal='video',
+            do_sample=False,
+        )
         print(f"Model response: {output_text}")
         end = time.perf_counter()
         print("Inference End")
